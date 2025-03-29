4456be64dd09f95688588e238a5d98db4c0a04ca
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-03-28 17:47:08 -0700

    Added main function and skipWhiteSpace helper

--- src/lexer/Tokenizer.js
+++ src/lexer/Tokenizer.js
@@ -1,39 +1,38 @@
+const { ColonToken } = require("./tokens/SymbolTokens");
+const { ClassNameTypeToken } = require("./tokens/TypeTokens");
+const { IntegerToken, StringToken } = require("./tokens/ExpressionTypeTokens");
+const VariableToken = require("./tokens/VariableToken");
 const {
-    ColonToken,
-  } = require("./tokens/SymbolTokens");
-  const {
-    ClassNameTypeToken,
-  } = require("./tokens/TypeTokens");
-  const {
-    IntegerToken,
-    StringToken,
-  } = require("./tokens/ExpressionTypeTokens");
-  const VariableToken = require("./tokens/VariableToken");
-  const {
-    ClassToken,
-    NewToken,
-    MethodNameToken,
-    ExtendToken,
-  } = require("./tokens/SpecialTokens");
-  const { keywordMap, symbolMap, multiCharSymbolMap } = require("../utils/Lexer");
+  ClassToken,
+  NewToken,
+  MethodNameToken,
+  ExtendToken,
+} = require("./tokens/SpecialTokens");
+const { keywordMap, symbolMap, multiCharSymbolMap } = require("../utils/Lexer");
 
-  class Tokenizer {
-    constructor(input) {
-      this.input = input;
-      this.offset = 0;
-      this.tokens = [];
+class Tokenizer {
+  constructor(input) {
+    this.input = input;
+    this.offset = 0;
+    this.tokens = [];
+  }
+
+  tokenizeAll() {
+    while (this.offset < this.input.length) {
+      const token = this.nextToken();
+      if (token !== null) this.tokens.push(token);
     }
+    return this.tokens;
+  }
 
-    tokenizeAll() {
+  nextToken() {}
 
+  skipWhiteSpace() {
+    while (
+      this.offset < this.input.length &&
+      /\s/.test(this.input[this.offset])
+    ) {
+      this.offset++;
     }
-
-    nextToken() {
-
-    }
-    skipWhiteSpace() {
-
-    }
-
-
+  }
 }
