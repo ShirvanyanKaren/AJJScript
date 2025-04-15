ce0e637a491e01c78c39f23c13b16fefa7cee132
ashgharibyan <ashghcode@gmail.com>
2025-04-14 23:58:33 -0700

    constructor node and class def tests

--- src/tests/Parser.test.js
+++ src/tests/Parser.test.js
@@ -18,5 +18,29 @@ describe('AST Nodes', () => {
         expect(program.statements).toEqual(statements);
     });
 
+    test('should create a ClassDef node', () => {
+        const name = 'MyClass';
+        const superclass = 'BaseClass';
+        const varDecs = ['varDec1'];
+        const constructorNode = 'constructorNode';
+        const methods = ['method1', 'method2'];
+        const classDef = new ClassDef(name, superclass, varDecs, constructorNode, methods);
+        expect(classDef.name).toBe(name);
+        expect(classDef.superclass).toBe(superclass);
+        expect(classDef.varDecs).toEqual(varDecs);
+        expect(classDef.constructor).toBe(constructorNode);
+        expect(classDef.methods).toEqual(methods);
+    });
+
+    test('should create a Constructor node', () => {
+        const params = ['param1', 'param2'];
+        const superCall = { arguments: ['arg1'] };
+        const body = ['stmt'];
+        const ctor = new Constructor(params, superCall, body);
+        expect(ctor.params).toEqual(params);
+        expect(ctor.superCall).toEqual(superCall);
+        expect(ctor.body).toEqual(body);
+    });
+
 
 }
