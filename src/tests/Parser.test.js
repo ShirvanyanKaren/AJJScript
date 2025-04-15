3c4aa70f2c118f8f34ebc66d9cea1ec11fbc8b99
ashgharibyan <ashghcode@gmail.com>
2025-04-14 23:57:21 -0700

    test module

--- src/tests/Parser.test.js
+++ src/tests/Parser.test.js
@@ -7,3 +7,16 @@ const {
     ExpressionStatement,
     BinaryExpression,
 } = require('../parser/ASTNodes');
+
+
+describe('AST Nodes', () => {
+    test('should create a Program node', () => {
+        const classDefs = ['classDef1', 'classDef2'];
+        const statements = ['stmt1', 'stmt2'];
+        const program = new Program(classDefs, statements);
+        expect(program.classDefs).toEqual(classDefs);
+        expect(program.statements).toEqual(statements);
+    });
+
+
+}
