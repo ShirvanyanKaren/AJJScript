de737ef166ba602347fca1551a899f7a11ac22a3
Narek-asa <narek604@gmail.com>
2025-05-17 13:56:03 -0700

    refactor of tokenizer

---
+++ src/lexer/tokens/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // Special Tokens
+    ...require("./SpecialTokens"),
+  
+    // Access Tokens
+    ...require("./AccessTokens"),
+  
+    // Symbol Tokens
+    ...require("./SymbolTokens"),
+  
+    // Operator Tokens
+    ...require("./OperatorTokens"),
+  
+    // Type Tokens
+    ...require("./TypeTokens"),
+  
+    // Expression Type Tokens
+    ...require("./ExpressionTypeTokens"),
+  
+    // Statement Tokens
+    ...require("./StatementTokens"),
+  
+    // Individual tokens
+    VariableToken: require("./VariableToken"),
+  };
+  
