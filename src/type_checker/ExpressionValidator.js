dbd1475df42076ceeb51ee5b8bf2f17934786c80
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 15:58:23 -0700

    init of TypeChecker utils

--- src/type_checker/ExpressionValidator.js
+++ src/type_checker/ExpressionValidator.js
@@ -1,6 +1,5 @@
 const TypeCheckerError = require("./TypeCheckerError");
-const {isTypeCompatible} = require("../utils/TypeChecker")
-const { isSubclass }  = require("../utils/TypeChecker");
+
 
 function typeCheckExpression(expr, ctx) {
   switch (expr.type) {
---
+++ src/utils/TyperChecker.js
@@ -0,0 +1,19 @@
+const TypeCheckerError = require("../type_checker/TypeCheckerError");
+
+function validateType(type, classTable) {
+
+}
+
+function isTypeCompatible(expected, actual, ctx) {
+
+}
+
+function isSubclass(sub, sup, ctx) {
+
+}
+
+module.exports = {
+  validateType,
+  isTypeCompatible,
+  isSubclass,
+};
