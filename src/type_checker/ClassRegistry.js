eeb7df340cdd4ab9bc7d737e69fdfd838d5313f2
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 14:59:14 -0700

    added class registry

---
+++ src/type_checker/ClassRegistry.js
@@ -0,0 +1,26 @@
+const TypeCheckerError = require("./TypeCheckerError");
+
+function buildClassRegistry(classDefs) {
+
+  const classTable = new Map();
+  const inheritanceMap = new Map();
+
+  if (classDefs.length === 0) {
+    return { classTable, inheritanceMap };
+  }
+  for (const classDef of classDefs) {
+    if (classTable.has(classDef.name)) {
+      throw TypeCheckerError.duplicateDefinition(classDef.name, "class", 0, 0);
+    }
+    classTable.set(classDef.name, classDef);
+
+    if (classDef.superclass) {
+      inheritanceMap.set(classDef.name, classDef.superclass);
+    }
+  }
+
+
+  return { classTable, inheritanceMap };
+}
+
+module.exports = { buildClassRegistry };
