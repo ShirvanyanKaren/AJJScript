fd5584ee14aea6a8d1de51d77f7a27a28c03c7be
Narek-asa <narek604@gmail.com>
2025-05-17 14:01:17 -0700

    added new ast nodes

--- src/parser/ASTNodes.js
+++ src/parser/ASTNodes.js
@@ -39,20 +39,117 @@ class VarDec {
     }
 }
 
-
 class ExpressionStatement {
     constructor(expression) {
+        this.type = "ExpressionStatement";
         this.expression = expression;
     }
 }
 
 class BinaryExpression {
     constructor(left, operator, right) {
+        this.type = "BinaryExpression";
         this.left = left;
         this.operator = operator;
         this.right = right;
     }
 }
+
+class UnaryExpression {
+    constructor(operator, operand) {
+        this.type = "UnaryExpression";
+        this.operator = operator;
+        this.operand = operand;
+    }
+}
+
+class MethodCall {
+    constructor(callee, methodName, args) {
+        this.type = "MethodCall";
+        this.callee = callee;
+        this.methodName = methodName;
+        this.args = args;
+    }
+}
+
+class FieldAccess {
+    constructor(object, field) {
+        this.type = "FieldAccess";
+        this.object = object;
+        this.field = field;
+    }
+}
+
+class This {
+    constructor() {
+        this.type = "This";
+    }
+}
+
+class Super {
+    constructor() {
+        this.type = "Super";
+    }
+}
+
+class NewExpression {
+    constructor(className, args) {
+        this.type = "NewExpression";
+        this.className = className;
+        this.args = args;
+    }
+}
+
+class ReturnStatement {
+    constructor(value) {
+        this.type = "Return";
+        this.value = value;
+    }
+}
+
+class Assignment {
+    constructor(left, right) {
+        this.type = "Assignment";
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Print {
+    constructor(argument) {
+        this.type = "Print";
+        this.argument = argument;
+    }
+}
+
+class TernaryExpression {
+    constructor(condition, trueExpr, falseExpr) {
+      this.type = "TernaryExpression";
+      this.condition = condition;
+      this.trueExpr = trueExpr;
+      this.falseExpr = falseExpr;
+    }
+  }
+
+  class MethodDeclaration {
+    constructor(name, params, returnType, body) {
+      this.type = "MethodDeclaration";
+      this.name = name;
+      this.params = params;
+      this.returnType = returnType;
+      this.body = body;
+    }
+  }
+  class ForLoop {
+    constructor(initializer, condition, increment, body) {
+      this.type = "ForLoop";
+      this.initializer = initializer;
+      this.condition = condition;
+      this.increment = increment;
+      this.body = body;
+    }
+  }  
+  
 
 module.exports = {
     Program,
@@ -62,4 +159,16 @@ module.exports = {
     VarDec,
     ExpressionStatement,
     BinaryExpression,
+    UnaryExpression,
+    MethodCall,
+    FieldAccess,
+    This,
+    Super,
+    NewExpression,
+    ReturnStatement,
+    Assignment,
+    Print,
+    TernaryExpression,
+    MethodDeclaration,
+    ForLoop,
 };
