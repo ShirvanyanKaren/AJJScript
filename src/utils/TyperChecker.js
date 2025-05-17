44e930496136d09e21517b00b8eba337c1b79b11
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 16:00:43 -0700

    isTypeCompatable and isSubclass utils

--- src/utils/TyperChecker.js
+++ src/utils/TyperChecker.js
@@ -7,13 +7,27 @@ function validateType(type, classTable) {
     }
   }
 
-function isTypeCompatible(expected, actual, ctx) {
+  function isTypeCompatible(expected, actual, ctx) {
+    if (expected.typeName === actual.typeName) return true;
+    if (actual.typeName === "null") {
+      return !["int", "boolean", "void"].includes(expected.typeName);
+    }
+    let current = expected.typeName
+    while (current) {
+      if (current == actual.typeName) return true;
+      if (!ctx?.inheritanceMap) break;
+      current = ctx.inheritanceMap.get(current);
+    }
+    return false;
+  }
 
-}
-
-function isSubclass(sub, sup, ctx) {
-
-}
+  function isSubclass(sub, sup, ctx) {
+    while (sub) {
+      if (sub === sup) return true;
+      sub = ctx.inheritanceMap.get(sub);
+    }
+    return false;
+  }
 
 module.exports = {
   validateType,
