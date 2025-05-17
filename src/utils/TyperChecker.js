86fa0ca762baf375f21c382a19dadd9f92035e61
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 15:59:00 -0700

    validate type util

--- src/utils/TyperChecker.js
+++ src/utils/TyperChecker.js
@@ -1,8 +1,11 @@
 const TypeCheckerError = require("../type_checker/TypeCheckerError");
 
 function validateType(type, classTable) {
-
-}
+    if (["integer", "string", "boolean", "void"].includes(type.typeName)) return;
+    if (!classTable.has(type.typeName)) {
+      throw TypeCheckerError.undefinedReference(type.typeName, "type", 0, 0);
+    }
+  }
 
 function isTypeCompatible(expected, actual, ctx) {
 
