46b3408e7a4084e84cad074fb619429f60d4ef0c
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 19:34:16 -0500

    finished implementation of Class mem validator

--- src/type_checker/ClassMemberValidator.js
+++ src/type_checker/ClassMemberValidator.js
@@ -1,8 +1,33 @@
 const TypeCheckerError = require("./TypeCheckerError");
-
+const { validateConstructor } = require("./ConstructorValifator");
+const { validateType } = require("../utils/TyperChecker");
+const { typeCheckExpression } = require("./ExpressionValidator");
 
 function validateClassMembers(classTable, inheritanceMap, ctx) {
+  for (const [name, classDef] of classTable.entries()) {
+    validateConstructor(classDef, ctx);
 
+    const fieldNames = new Set();
+    for (const varDec of classDef.varDecs) {
+      if (fieldNames.has(varDec.identifier)) {
+        throw TypeCheckerError.duplicateDefinition(varDec.identifier, "field", 0, 0);
+      }
+      fieldNames.add(varDec.identifier);
+      validateType(varDec.varType, classTable);
+    }
+
+    const methodNames = new Set();
+    for (const method of classDef.methods) {
+      if (methodNames.has(method.name)) {
+        throw TypeCheckerError.duplicateDefinition(method.name, "method", 0, 0);
+      }
+      methodNames.add(method.name);
+      validateType(method.returnType, classTable);
+      for (const param of method.params) {
+        validateType(param.varType, classTable);
+      }
+    }
+  }
 }
 
 
