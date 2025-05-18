b443b0057cd95fae4ddadb1f2ff7a365da5847cc
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 19:36:13 -0500

    Method body checker init module

---
+++ src/type_checker/MethodBodyChecker.js
@@ -0,0 +1,15 @@
+// src/typechecker/MethodBodyChecker.js
+const TypeCheckerError = require("./TypeCheckerError");
+
+
+function typeCheckMethodBodies(classTable, ctx) {
+
+}
+
+function typeCheckConstructorBody(constructor, ctx) {
+  
+}
+
+module.exports = {
+  typeCheckMethodBodies,
+};
