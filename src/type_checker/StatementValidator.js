e670c1c071c060d3fb604b8b5ab8933f176b6efb
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 19:42:30 -0500

    final cases for statement valid, expression, return , assingment

--- src/type_checker/StatementValidator.js
+++ src/type_checker/StatementValidator.js
@@ -1,7 +1,6 @@
-const TypeCheckerError = require("./TypeCheckError");
+const TypeCheckerError = require("./TypeCheckerError");
 const { typeCheckExpression } = require("./ExpressionValidator");
 const { isTypeCompatible } = require("../utils/TyperChecker");
-
 
 function typeCheckStatement(stmt, ctx) {
   const { currentMethod, currentClass, globalVars } = ctx;
@@ -39,6 +38,98 @@ function typeCheckStatement(stmt, ctx) {
       }
       break;
     }
+
+    case "Return": {
+      if (!currentMethod) {
+        throw new TypeCheckerError("Return statement outside of method", 0, 0);
+      }
+      const expectedType = currentMethod.returnType;
+      if (!stmt.value && expectedType.typeName !== "void") {
+        throw TypeCheckerError.returnTypeMismatch(
+          expectedType.typeName,
+          "void",
+          0,
+          0,
+        );
+      }
+      if (stmt.value) {
+        const actualType = typeCheckExpression(stmt.value, ctx);
+
+        if (!isTypeCompatible(expectedType, actualType, ctx)) {
+          throw TypeCheckerError.returnTypeMismatch(
+            expectedType.typeName,
+            actualType.typeName,
+            0,
+            0,
+          );
+        }
+      }
+      break;
+    }
+
+    case "Assignment": {
+      if (stmt.left.varType && stmt.left.identifier) {
+        const declaredType = stmt.left.varType;
+        const varName = stmt.left.identifier;
+        const valueType = typeCheckExpression(stmt.right, ctx);
+
+        if (!isTypeCompatible(declaredType, valueType, ctx)) {
+          throw TypeCheckerError.typeMismatch(
+            declaredType.typeName,
+            valueType.typeName,
+            0,
+            0,
+          );
+        }
+
+        if (!currentClass && !currentMethod) {
+          globalVars.set(varName, declaredType);
+        }
+
+        return;
+      }
+
+      const leftType = typeCheckExpression(stmt.left, ctx);
+      const rightType = typeCheckExpression(stmt.right, ctx);
+      if (!isTypeCompatible(leftType, rightType, ctx)) {
+        throw TypeCheckerError.typeMismatch(
+          leftType.typeName,
+          rightType.typeName,
+          0,
+          0,
+        );
+      }
+      return;
+    }
+    case "ExpressionStatement": {
+      if (stmt?.expression?.type === "MethodDeclaration") {
+        const oldClass = ctx.currentClass;
+        const oldMethod = ctx.currentMethod;
+
+        ctx.currentMethod = {
+          params: stmt.expression.params,
+          returnType: stmt.expression.returnType,
+          name: stmt.expression.name,
+        };
+        ctx.globalMethods.set(stmt.expression.name, ctx.currentMethod);
+
+        for (const stmts of stmt.expression.body.statements) {
+          typeCheckStatement(stmts, ctx);
+        }
+
+        ctx.currentClass = oldClass;
+        ctx.currentMethod = oldMethod;
+        break;
+      } else {
+        typeCheckExpression(stmt.expression, ctx);
+      }
+      break;
+    }
+
+    default:
+      if (stmt.expression) {
+        typeCheckExpression(stmt.expression, ctx);
+      }
   }
 }
 
