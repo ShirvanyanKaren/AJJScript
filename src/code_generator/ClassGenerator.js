41179268d3e97656ed4826e62cb9f1da01688471
ashgharibyan <ashghcode@gmail.com>
2025-05-17 21:28:47 -0500

    finished logic for class gen

--- src/code_generator/ClassGenerator.js
+++ src/code_generator/ClassGenerator.js
@@ -18,7 +18,57 @@ function generateClassDef(classDef, ctx) {
   
     ctx.currentClass = classDef.name;
   
-
+    // Class declaration
+    if (classDef.superclass) {
+      appendLine(
+        `const ${classDef.name} = AJJ.createClass('${classDef.name}', ${classDef.superclass});`
+      );
+    } else {
+      appendLine(`const ${classDef.name} = AJJ.createClass('${classDef.name}');`);
+    }
+  
+    // Constructor
+    const params = classDef.constructor?.params || [];
+    appendLine(
+      `${classDef.name}.prototype.init = function(${generateParams(params)}) {`
+    );
+    indentLevelCallback(1);
+  
+    for (const param of params) declaredVariables.add(param.identifier);
+    for (const varDec of classDef.varDecs || []) {
+      appendLine(`this.${varDec.identifier} = null;`);
+    }
+  
+    if (classDef.constructor?.superCall) {
+      const superClass = classDef.superclass && classTable.get(classDef.superclass);
+      const superParamCount = superClass?.constructor?.params?.length || 0;
+      const superArgs = params.slice(0, superParamCount).map(p => p.identifier).join(", ");
+      appendLine(`Object.getPrototypeOf(${classDef.name}.prototype).init.call(this, ${superArgs});`);
+    }
+  
+    for (const stmt of classDef.constructor?.body || []) {
+      generateStatement(stmt, ctx);
+    }
+  
+    indentLevelCallback(-1);
+    appendLine("};");
+  
+    // Methods
+    for (const method of classDef.methods) {
+      appendLine(`${classDef.name}.prototype.${method.name} = function(${generateParams(method.params)}) {`);
+      indentLevelCallback(1);
+  
+      for (const param of method.params) declaredVariables.add(param.identifier);
+      for (const stmt of method.body) {
+        generateStatement(stmt, ctx);
+      }
+  
+      indentLevelCallback(-1);
+      appendLine("};");
+      declaredVariables.clear();
+    }
+  
+    ctx.currentClass = null;
   }
   
   module.exports = { generateClassDef };
---
+++ src/code_generator/ExpressionGenerator.js
@@ -0,0 +1,12 @@
+/**
+ * ExpressionGenerator.js
+ *
+ * Handles expression code generation for AJJScript.
+ */
+
+function generateExpression(expr, ctx) {
+    
+  }
+  
+  module.exports = { generateExpression };
+  
