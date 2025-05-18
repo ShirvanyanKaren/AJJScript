c71510b1a33e91198b2be690cd38b2f8317c4a56
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2025-05-17 19:49:36 -0500

    finished method body checker

--- src/type_checker/MethodBodyChecker.js
+++ src/type_checker/MethodBodyChecker.js
@@ -1,13 +1,34 @@
 // src/typechecker/MethodBodyChecker.js
-const TypeCheckerError = require("./TypeCheckerError");
-
+const TypeCheckerError = require("./TypeCheckError");
+const { typeCheckStatement } = require("./StatementValidator");
+const { typeCheckSuperCall } = require("./SuperCallValidator");
 
 function typeCheckMethodBodies(classTable, ctx) {
+  for (const classDef of classTable.values()) {
+    ctx.currentClass = classDef;
 
+    typeCheckConstructorBody(classDef.constructor, ctx);
+
+    for (const method of classDef.methods) {
+      ctx.currentMethod = method;
+      for (const stmt of method.body) {
+        typeCheckStatement(stmt, ctx);
+      }
+    }
+
+    ctx.currentClass = null;
+    ctx.currentMethod = null;
+  }
 }
 
 function typeCheckConstructorBody(constructor, ctx) {
-  
+  if (constructor.superCall) {
+    typeCheckSuperCall(constructor.superCall, ctx);
+  }
+
+  for (const stmt of constructor.body) {
+    typeCheckStatement(stmt, ctx);
+  }
 }
 
 module.exports = {
